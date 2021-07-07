655a0094fbb43434b571587453f7ebe26f6987f1
Bảo Bảo <44540022+Thanh-Bao@users.noreply.github.com>
2021-07-07 09:09:58 +0700

    Revert "cart function component"
    This reverts commit fc38e92e6285a90c2017350d2b370acceb2ced53.

--- src/component/CustomCard.js
+++ src/component/CustomCard.js
@@ -3,54 +3,49 @@ import { Text, View, Image } from 'react-native'
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
 import { decode } from 'he'
 
-// class CustomCard extends Component {
-//     render() {
-//         var striptags = require('striptags');
-//         var numeral = require('numeral');
-//         return (
+class CustomCard extends Component {
+    render() {
+        var striptags = require('striptags');
+        var numeral = require('numeral');
+        return (
+            <View>
+                <Card>
+                    <Card.Title style={{ fontSize: 30 }}>{this.props.name}</Card.Title>
+                    <Card.Divider />
+                    <Card.Image source={{ uri: `${this.props.thumbnail}` }}>
+                    </Card.Image>
 
 
-//        )
-//     }
-// }
+                    <View style={{flexDirection: 'row',justifyContent:'center', flex: 1}}>
+                        <Text style={{fontSize: 30, fontWeight: '700', color:'red'}}>
+                        {numeral(this.props.price).format('0,0')} đ
+                        </Text>
+                    </View>
 
-function CustomCard(props) {
-    var striptags = require('striptags');
-    var numeral = require('numeral');
-    return (
-        <View>
-            <Card>
-                <Card.Title style={{ fontSize: 30 }}>{props.name}</Card.Title>
-                <Card.Divider />
-                <Card.Image source={{ uri: `${props.thumbnail}` }}>
-                </Card.Image>
-                <View style={{ flexDirection: 'row', justifyContent: 'center', flex: 1 }}>
-                    <Text style={{ fontSize: 30, fontWeight: '700', color: 'red' }}>
-                        {numeral(props.price).format('0,0')} đ
+                    <Text
+                        style={{ marginBottom: 10 }}
+                        numberOfLines={3}
+                    >
+                        {decode(striptags(this.props.description))}
+
                     </Text>
-                </View>
-                <Text
-                    style={{ marginBottom: 10 }}
-                    numberOfLines={3}
-                >
-                    {decode(striptags(props.description))}
+                    <View style={{ flexDirection: 'row', justifyContent: 'center', flex: 1 }}>
+                        <Button
 
-                </Text>
-                <View style={{ flexDirection: 'row', justifyContent: 'center', flex: 1 }}>
-                    <Button
-                        type="outline"
-                        icon={<Icon name='visibility' color='#0066ff' />}
-                        buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 10, marginBottom: 0 }}
-                        title=' Chi tiết' />
+                            type="outline"
+                            icon={<Icon name='visibility' color='#0066ff' />}
+                            buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 10, marginBottom: 0 }}
+                            title=' Xem chi tiết' />
 
-                    <Button
-                        icon={<Icon name='add-shopping-cart' color='#ffffff' />}
-                        buttonStyle={{ borderRadius: 0, marginLeft: 10, marginRight: 0, marginBottom: 0 }}
-                        title='Mua' />
-                </View>
-            </Card>
-        </View>
-    )
+                        <Button
+                            icon={<Icon name='add-shopping-cart' color='#ffffff' />}
+                            buttonStyle={{ borderRadius: 0, marginLeft: 10, marginRight: 0, marginBottom: 0 }}
+                            title=' Thêm vào giỏ hàng' />
+                    </View>
+                </Card>
+            </View>
+        )
+    }
 }
 
 export default CustomCard;
--- src/screen/AccountContent.js
+++ src/screen/AccountContent.js
@@ -1,27 +1,87 @@
+// import React, { Component } from 'react'
+
+
+// import { View, Text, Image } from 'react-native'
+// import { Card, ListItem, Button, Icon } from 'react-native-elements'
+
+
+
+// class AccountContent extends Component {
+
+//     render() {
+
+//         return (
+//             <View style={{flexDirection:'row'}}>
+//                 <Text> Tài khoản  </Text>
+//                 <Text> Tài khoản  </Text>
+//             </View>
+//         )
+//     }
+// }
+
+
+// export default AccountContent;
+
+
 import React, { Component } from 'react'
+import { StyleSheet, FlatList, Text, View } from 'react-native'
+import axios from 'axios';
+
+export default class AccountContent extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [],
+            page: 1
+        }
+    }
 
 
-import { View, Text, Image } from 'react-native'
-import { Card, ListItem, Button, Icon } from 'react-native-elements'
+    componentDidMount() {
+        this.getData();
+    }
 
-
-
-class AccountContent extends Component {
-
-    render() {
-
+    getData = async () => {
+        axios({
+            method: 'get',
+            url: 'https://jsonplaceholder.typicode.com/photos?_limit=10&_page=' + this.state.page
+        }).then((respnse) => {
+            console.log(respnse.data)
+            this.setState({
+                data: this.state.data.concat(respnse.data)
+            }
+            )
+        }).catch((err) => {
+            alert('lỗi lấy danh sách sản phẩm')
+        })
+    }
+    renderRow = ({ item }) => {
         return (
-            <View style={{flexDirection:'row'}}>
-                <Text> Tài khoản  </Text>
-                <Text> Tài khoản  </Text>
+            <View>
+                <Text style={{marginBottom: 90}}>{item.id}</Text>
             </View>
         )
     }
+
+    handleLoadMore = () => {
+        this.setState({
+            page: this.state.page + 1
+        }, this.getData
+        )
+    }
+
+    render() {
+        return (
+            <FlatList
+                data={this.state.data}
+                renderItem={this.renderRow}
+                keyExtractor={(item, index) => index.toString()}
+                onEndReached={this.handleLoadMore}
+            />
+        )
+    }
 }
-
-
-export default AccountContent;
-
 
 
 
