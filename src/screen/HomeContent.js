395768d8f15114b8f29eee45aa84627bedfb9e97
Bảo Bảo <44540022+Thanh-Bao@users.noreply.github.com>
2021-07-07 09:09:43 +0700

    Revert "change to function component"
    This reverts commit a53cc954604fac471bcca84503a3ece80a8d66f1.

--- src/screen/HomeContent.js
+++ src/screen/HomeContent.js
@@ -1,54 +1,68 @@
-import React, { Component, useState, useEffect } from 'react'
-import { Text, View, Image, FlatList, Button } from 'react-native'
+import React, { Component } from 'react'
+import { Text, View, Image, FlatList } from 'react-native'
 import axios from 'axios';
-import { useNavigation } from '@react-navigation/native'
 
 import CustomCard from '../component/CustomCard'
 
-function HomeContent() {
-    const [listProduct, setListProduct] = useState([]);
-    const [page, setPage] = useState(1);
+class HomeContent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            listProduct: [],
+            page: 1
+        }
+    }
 
 
-    function getData() {
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData = async () => {
         axios({
             method: 'get',
-            url: 'https://baobaoshop.live/api/Products?page=' + page
+            url: 'https://baobaoshop.live/api/Products?page=' + this.state.page
         }).then((respnse) => {
-            setListProduct(listProduct.concat(respnse.data.list));
+            this.setState({
+                listProduct: this.state.listProduct.concat(respnse.data.list)
+            })
         }).catch((err) => {
             alert('lỗi lấy danh sách sản phẩm')
         })
     }
-    function handleLoadMore() {
-        setPage(page + 1);
-        getData();
 
+
+    handleLoadMore = () => {
+        this.setState({
+            page: this.state.page + 1
+        }, this.getData
+        )
     }
 
 
-    useEffect(() => {
-        getData();
-    }, []);
-
-
-    return (
-        <View>
-            <FlatList style={{ marginBottom: 283 }}
-                data={listProduct}
-                renderItem={({ item, navigation }) =>
-                    <CustomCard
-                        navigation={navigation}
-                        name={item.name}
-                        thumbnail={item.thumbnail}
-                        description={item.description}
-                        price={item.price}
-                    />
-                }
-                keyExtractor={(item, index) => index.toString()}
-                onEndReached={handleLoadMore}
-            />
-        </View>
-    )
+    render() {
+        return (
+            <View>
+                <FlatList style={{ marginBottom: 283 }}
+                    data={this.state.listProduct}
+                    renderItem={({ item }) =>
+                        <CustomCard
+                            name={item.name}
+                            thumbnail={item.thumbnail}
+                            description={item.description}
+                            price={item.price}
+                        />
+                    }
+                    keyExtractor={(item, index) => index.toString()}
+                    onEndReached={this.handleLoadMore}
+                />
+            </View>
+        )
+    }
 }
+
+
+
 export default HomeContent;
